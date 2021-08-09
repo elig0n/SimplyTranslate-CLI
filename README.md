d3a1263ccc203e3f193df4a9bff0c2928c641646
fattalion <fattalion@disroot.org>
2021-08-09 17:23:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SimplyTranslate CLI
+
+This is a simple CLI for SimplyTranslate.
+
+## Contact
+
+To get in contact with the developers, visit us on the #simple-web IRC channel on [Libera.Chat](https://libera.chat).
+
+## License
+
+simplytranslate_cli is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+simplytranslate_cli is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
---
+++ main.py
@@ -0,0 +1,48 @@
+import argparse
+
+from simplytranslate_engines.libretranslate import LibreTranslateEngine
+from simplytranslate_engines.googletranslate import GoogleTranslateEngine
+from simplytranslate_engines.utils import to_lang_code
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("text", metavar="TEXT", nargs="+", help="The text to translate")
+
+parser.add_argument(
+    "-e",
+    "--engine",
+    choices=("google", "libre"),
+    default="google",
+    help="Translation engine to use",
+)
+parser.add_argument("-i", "--instance", help="Instance URL to use (only for libre)")
+
+parser.add_argument("-f", "--from", default="auto", help="Language to translate from")
+parser.add_argument("-t", "--to", required=True, help="Language to translate to")
+
+args = vars(parser.parse_args())
+
+engine_name = args["engine"]
+instance = args.get("instance")
+
+if engine_name == "libre":
+    if instance is None:
+        instance = "https://libretranslate.de"
+    elif not (instance.startswith("https://") or instance.startswith("http://")):
+        instance = f"https://{instance}"
+
+    engine = LibreTranslateEngine(instance)
+elif engine_name == "google":
+    if instance is not None:
+        parser.error("You can't set instance for Google Translate")
+
+    engine = GoogleTranslateEngine()
+
+from_language = to_lang_code(args["from"], engine)
+to_language = to_lang_code(args["to"], engine)
+
+print(
+    engine.translate(
+        " ".join(args["text"]), from_language=from_language, to_language=to_language
+    )
+)
